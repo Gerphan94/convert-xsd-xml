ac7aa84e230e2d16c63775c33e33af8dac852760
Fan Duc'k <gerphan94@gmail.com>
2025-10-02 09:19:24 +0700

    FIX IN HOA

--- src/components/firebase/fb-parent.js
+++ src/components/firebase/fb-parent.js
@@ -1,16 +1,8 @@
 import { ref, set, get, child } from "firebase/database";
 import { db } from "../../config/firebase";
+import { toSnakeCase } from "../func";
 // utils/convert.js
-export const toSnakeCase = (str) => {
-    return str
-        .normalize("NFD")                   // split accents from letters
-        .replace(/[\u0300-\u036f]/g, "")    // remove accents
-        .toLowerCase()                      // convert to lowercase
-        .replace(/đ/g, "d")                 // replace special Vietnamese char
-        .replace(/[^a-z0-9\s]/g, "")        // remove non-alphanumeric
-        .trim()                             // remove spaces at ends
-        .replace(/\s+/g, "_");              // replace spaces with underscores
-};
+
 
 
 export const writeParent = async (name, topicid) => {
